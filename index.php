d8872b979a5edf30458502c3dbd26656ed452602
Ving Trung <vtrung>
2016-07-26 07:43:15 -0700

    all files

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>EducateNet</title>
+
+    <!-- Bootstrap -->
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  </head>
+
+  <body>
+    <header>
+      <div class="container">
+        <h1>EducationNet</h1>
+      </div>
+    </header>
+    <div class="main-container container">
+      <div class="row">
+        <section class="lesson-container col-sm-3">
+          lesson-container
+        </section>
+        <section class="content-container col-sm-6">
+          content-container
+          <div class="container-fluid">
+            <div class="row">
+              <div class="col-lg-8">
+
+              </div>
+              <div class="col-lg-4">
+
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="nav-container col-sm-3">
+          nav-container
+        </section>
+      </div>
+    </div>
+    <style>
+      header{
+        /*background: #E9EBEE;*/
+        background: #3B5998;
+        color: #FFFFFF;
+      }
+      section{
+        border:1px solid black;
+        float:left;
+        height: 600px;
+      }
+
+      .main-container{
+        overflow: auto;
+      }
+    </style>
+  </body>
+
+</html>
