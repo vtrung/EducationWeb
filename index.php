5e480767d56561a0cdfee4a300ad797cbf249d91
Max MacEachern <maceachm@oregonstate.edu>
2016-07-28 19:19:42 -0700

    Added PHP header

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
+<?php
+//Turn on error reporting
+ini_set('display_errors', 'On');
+//Connects to the database
+$mysqli = new mysqli("oniddb.cws.oregonstate.edu","cooperph-db","NKWCQdcXNLL2X6T6","cooperph-db");
+if($mysqli->connect_errno){
+echo "Connection error " . $mysqli->connect_errno . " " . $mysqli->connect_error;
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
